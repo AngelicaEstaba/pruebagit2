618012ed60e106dcb55fe03afc56c9b24f25f004
AngelicaEstaba <angelica.94.24.arg@gmail.com>
2023-04-12 20:32:21 -0300

    Inicio del Proyecto

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Agustin
+ *
+ */
+module ProyectoGit {
+}
---
+++ src/pruebagit/ProyectoGit.java
@@ -0,0 +1,10 @@
+package pruebagit;
+
+public class ProyectoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
